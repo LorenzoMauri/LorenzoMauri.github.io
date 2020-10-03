1b56210d2d488970a7796116ed39fa62f1505d73
LorenzoMauri <57866987+LorenzoMauri@users.noreply.github.com>
2020-10-03 09:10:40 +0200

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lorenzo Mauri</title>
+    <link rel="stylesheet" href="styling.css">
+   
+</head>
+
+<body>
+
+<h1>  
+Il mio sito    
+</h1>
+
+<div>
+
+<?php include 'file2.php' ?>
+
+</div>
+
+ 
+</body>
+</html>
+
+
