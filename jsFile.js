6817ed5b27d68478f969a48a5f4959975c9f0d6b
Lorenzo Mauri <lorenzo.mauri@ipratico.it>
2021-01-22 10:15:45 +0100

    update

--- jsFile.js
+++ jsFile.js
@@ -1,8 +1,8 @@
-function initViz() {
-    var containerDiv = document.getElementsByClassName("vizContainer"),
-    url = "https://public.tableau.com/profile/lorenzo8599#!/vizhome/VIZ_16110635714310/Dashboard1?publish=yes";
-
-    var viz = new tableau.Viz(containerDiv, url);
-};
-
-initViz();
+var divElement = document.getElementById('viz1611306008523');       
+                       var vizElement = divElement.getElementsByTagName('object')[0];            
+                        if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1366px';vizElement.style.height='795px';} 
+                        else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1366px';vizElement.style.height='795px';}
+                        else { vizElement.style.width='100%';vizElement.style.height='1127px';}              
+                       var scriptElement = document.createElement('script');                 
+                       scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+                        vizElement.parentNode.insertBefore(scriptElement, vizElement);   
