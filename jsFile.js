744c563eb2e025c1c95bb8c08b2ffb90985b40d5
Lorenzo Mauri <lorenzo.mauri@ipratico.it>
2021-01-22 09:55:37 +0100

    update

--- jsFile.js
+++ jsFile.js
@@ -1,5 +1,5 @@
 function initViz() {
-    var containerDiv = document.getElementById("vizContainer"),
+    var containerDiv = document.getElementsByClassName("vizContainer"),
     url = "https://public.tableau.com/profile/lorenzo8599#!/vizhome/VIZ_16110635714310/Dashboard1?publish=yes";
 
     var viz = new tableau.Viz(containerDiv, url);
